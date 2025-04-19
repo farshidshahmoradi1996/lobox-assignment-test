aeee758751a5de33463613b26a2139037b64647c
Farshid Shahmoradi <farshidshahmoradi1996@gmail.com>
2025-04-19 23:59:27 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { MultiSelect } from '@/components';
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <p>This is a assement test</p>
+
+      <MultiSelect />
+    </div>
+  );
+}
+
+export default App;
